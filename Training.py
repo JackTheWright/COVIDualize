890ea8ca47ae1fe760fea87ecc8ee5d88ca44d8f
Alex Brown <43386112+ajwb@users.noreply.github.com>
2020-04-20 13:55:56 -0700

    updates to training

--- Training.py
+++ Training.py
@@ -3,11 +3,30 @@ import altair as alt
 import numpy as np
 import math
 from sklearn import linear_model, preprocessing, model_selection
-# from statsmodels.tsa.arima_model import ARIMA
 import datetime
 
 
-def expoIncrease(predict_df, forecast_set, inpSize):
+def expoIncrease(predict_df, forecast_set):
+    last_date = "2020-04-17"
+    last_date = datetime.datetime.strptime(last_date, "%Y-%m-%d")
+    last_unix = last_date.timestamp()
+    one_day = 86400
+    next_unix = last_unix + one_day
+    index = 87
+    diff = predict_df.loc[index - 1][1] - forecast_set[0]
+    mult = 1.02
+
+    for i in forecast_set:
+        next_date = datetime.datetime.fromtimestamp(next_unix)
+        next_unix += one_day
+        predict_df.loc[index] = [str(next_date)] + [mult*(i + diff)]
+        index += 1
+        mult *= 1.03
+
+    chart(predict_df)
+
+
+def continueTrend(predict_df, forecast_set):
     last_date = "2020-04-17"
     last_date = datetime.datetime.strptime(last_date, "%Y-%m-%d")
     last_unix = last_date.timestamp()
@@ -20,14 +39,14 @@ def expoIncrease(predict_df, forecast_set, inpSize):
     for i in forecast_set:
         next_date = datetime.datetime.fromtimestamp(next_unix)
         next_unix += one_day
-        predict_df.loc[index] = [str(next_date)] + [mult*(i + diff + 500)]
+        predict_df.loc[index] = [str(next_date)] + [mult*(i + diff)]
         index += 1
-        mult *= 1.03
+        mult *= 0.988
 
     chart(predict_df)
 
 
-def levelOff(predict_df, forecast_set, inpSize):
+def levelOff(predict_df, forecast_set):
     last_date = "2020-04-17"
     last_date = datetime.datetime.strptime(last_date, "%Y-%m-%d")
     last_unix = last_date.timestamp()
@@ -38,19 +57,6 @@ def levelOff(predict_df, forecast_set, inpSize):
     mult = 1
 
     for i in forecast_set:
-        if (inpSize == 60):
-            if (inpSize == 90):
-                if (inpSize == 120):
-                    if (index == 87 + 90):
-                        diff = predict_df.loc[index - 1][1] - \
-                            forecast_set[90]
-                if (index == 87 + 60):
-                    diff = predict_df.loc[index - 1][1] - forecast_set[60]
-            if (index == 87 + 30):
-                print(len(predict_df))
-                diff = predict_df.loc[index - 1][1] - forecast_set[30]
-
-        print(diff)
         next_date = datetime.datetime.fromtimestamp(next_unix)
         next_unix += one_day
         predict_df.loc[index] = [str(next_date)] + [mult*(i + diff)]
@@ -99,59 +105,11 @@ def trainData(listDF):
     active_df = active_df.merge(covidPE_df, on='Date')
     active_df = active_df.merge(covidQC_df, on='Date')
     active_df = active_df.merge(covidSA_df, on='Date')
-
-    # X = np.array(active_df.drop(['Active_Cases', 'Date'], axis=1))
-    # y = np.array(active_df['Active_Cases'])
-
-    # clf = ARIMA(y, (1, 1, 1))
-    # clf_fit = clf.fit(disp=0)
-
-    # start_index = len(y)
-    # end_index = start_index + 6
-    # forecast = clf_fit.predict(start=start_index, end=end_index)
-
-    # history = [x for x in y]
-    # day = 1
-    # for yhat in forecast:
-    #     print('Day %d: %f' % (day, yhat))
-    #     history.append(yhat)
-    #     day += 1
-
-    # predict_df = active_df.drop(
-    #     ['Cases', 'Cases_x', 'Cases_y', 'Deaths', 'Recoveries'], 1)
-
-    # last_date = "2020-04-17"
-    # last_date = datetime.datetime.strptime(last_date, "%Y-%m-%d")
-    # last_unix = last_date.timestamp()
-    # one_day = 86400
-    # next_unix = last_unix + one_day
-    # index = 87
-
-    # for i in forecast:
-    #     next_date = datetime.datetime.fromtimestamp(next_unix)
-    #     next_unix += one_day
-    #     predict_df.loc[index] = [str(next_date)] + [i]
-    #     index += 1
-
-    # chart = alt.Chart(predict_df).mark_line().encode(
-    #     x='Date',
-    #     y='Active_Cases'
-    # ).interactive()
-
-    # chart.show()
-
-    # forecast_col = 'Active_Cases'
-    # forecast_out = int(math.ceil(0.01*len(active_df)))
-
-    # active_df['Lag_Case_1'] = active_df['Active_Cases'].shift(-1)
-    # active_df['Lag_Case_2'] = active_df['Active_Cases'].shift(-2)
-    # active_df['Lag'] = active_df[forecast_col].shift(-forecast_out)
     active_df.dropna(inplace=True)
 
     X = np.array(active_df.drop(['Active_Cases', 'Date'], axis=1))
     y = np.array(active_df['Active_Cases'])
     X = preprocessing.scale(X)
-    # X_predict = X[-forecast_out:]
 
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X, y, test_size=0.1)
@@ -161,19 +119,14 @@ def trainData(listDF):
     accuracy = clf.score(X_test, y_test)
     print(accuracy)
 
-    # forecast_set = clf.predict(X_predict)
-    # forecast_set = predict(clf, active_df, X, 30, 30)
     forecast_out = 30
     X_predict = X[-forecast_out:]
-    # print(X_predict)
-
-    X_predict = np.append(X_predict, X_predict, axis=0)
     forecast_set = clf.predict(X_predict)
 
     predict_df = active_df.drop(
         ['Cases', 'Cases_x', 'Cases_y', 'Deaths', 'Recoveries', ], 1)
 
     # expoIncrease(predict_df, forecast_set)
-    levelOff(predict_df, forecast_set, 60)
+    levelOff(predict_df, forecast_set)
 
     return
